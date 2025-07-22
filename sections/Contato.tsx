d40a5a1ac45cbb57f21bdbd06061dc59bea73b4b
Augusto Barbosa <augustocbarbosa@gmail.com>
2025-07-22 16:09:57 -0300

    Publish 5 from localhost into origin/main

---
+++ sections/Contato.tsx
@@ -0,0 +1,59 @@
+export interface Props {
+  form?: {
+    title: string;
+    name: string;
+    email: string;
+    message: string;
+    submit: string;
+  }
+  email?: {
+    title: string;
+    email: string;
+  }
+}
+
+export default function Contato({
+  form = {
+    title: "Envie uma mensagem",
+    name: "Nome",
+    email: "Email",
+    message: "Mensagem",
+    submit: "Enviar",
+  },
+  email = {
+    title: "Nosso email",
+    email: "contato@lemelab.org",
+  },
+}: Props) {
+  return (
+    <div className="container px-12 flex gap-28 my-12">
+      <div className="flex gap-28">
+        <div className="w-96 flex flex-col gap-5">
+          <h3 className="font-serif text-2xl">{form?.title}</h3>
+          <form className="flex flex-col gap-5">
+            <label className="flex flex-col gap-2">
+              <div>{form?.name}</div>
+              <input type="text" className="px-3 py-2 border border-primary rounded-md bg-base-100 focus:bg-white" />
+            </label>
+            <label className="flex flex-col gap-2">
+              <div>{form?.email}</div>
+              <input type="text" className="px-3 py-2 border border-primary rounded-md bg-base-100 focus:bg-white" />
+            </label>
+            <label className="flex flex-col gap-2">
+              <div>{form?.message}</div>
+              <textarea className="px-3 py-2 border border-primary rounded-md bg-base-100 focus:bg-white h-32">
+              </textarea>
+            </label>
+            <div className="flex justify-end">
+              <button className="flex space-x-4 align-middle bg-base-100 text-primary hover:bg-primary hover:text-white transition-colors duration-200 font-semibold py-2 px-4 border-2 border-secondary rounded-md shadow-[2px_2px_0_rgba(234,115,66,1)]">{form?.submit}</button>
+            </div>
+          </form>
+        </div>
+        <div className="w-96 flex flex-col gap-5">
+          <h3 className="font-serif text-2xl">{email?.title}</h3>
+          <p>{email?.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
