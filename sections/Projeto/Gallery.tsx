3df98f57249b4c5f59aecd4d55178ca982ecf114
Augusto Barbosa <augustocbarbosa@gmail.com>
2025-08-14 19:10:27 -0300

    Publish 2 from localhost into origin/main

--- sections/Projeto/Gallery.tsx
+++ sections/Projeto/Gallery.tsx
@@ -22,7 +22,7 @@ export default function Gallery({
               <div className="text-primary">
                 <Title label="Galeria" titleSize="2xl" serif />
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {/* Mobile */}
                 {images?.map((image) => (
                   <div className="md:hidden">
